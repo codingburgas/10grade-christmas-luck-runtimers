d5a5ca1ccabe793fac92a0dd46ed48053885b625
ZVYankova22 <ZVYankova22@codingburgas.bg>
2024-12-16 20:42:33 +0200

    Rearange files and create a dev branch

---
+++ Runtimers/inventory.h
@@ -0,0 +1,18 @@
+#include "pch.h"
+
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+struct Chemical
+{
+	string name;
+	string casNumber;
+	string chemicalFormula;
+	int quantity;
+	string expiryDate;
+	int hazardLevel; // 1 -> ниско , 5 -> високо
+};
+void loadInventory(const string& fileName, vector<Chemical>& inventory);
+void saveInventory(const string& fileName, vector<Chemical>& inventory);
+void displayInventory(const vector<Chemical>& inventory);
+#endif
