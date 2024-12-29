7e9fb51dc2d4a409bd8c0df427a0208cc70ac9d5
ZVYankova22 <ZVYankova22@codingburgas.bg>
2024-12-29 18:00:58 +0200

    Test raylib set up

--- app/Runtimers/main.cpp
+++ app/Runtimers/main.cpp
@@ -1,8 +1,18 @@
-#include "pch.h"
-#include "inventory.h"
+#include "raylib.h"
 
-int main() {
-	vector<Chemical>inventory;
-	loadInventory("chemicals.txt", inventory);
+int main(void)
+{
+    InitWindow(800, 450, "raylib [core] example - basic window");
 
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
