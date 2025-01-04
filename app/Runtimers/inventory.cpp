b2b1fb5613913889568464114a58481cbcf8488f
ZVYankova22 <ZVYankova22@codingburgas.bg>
2025-01-04 18:25:43 +0200

    Display reagents' information in the inventory room, fix exit button pos

--- app/Runtimers/inventory.cpp
+++ app/Runtimers/inventory.cpp
@@ -110,7 +110,7 @@ void displayChembenchZoomed()
 
     Vector2 exitButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
 
-    const Rectangle exitButton = { exitButtonPosition.x + 90, exitButtonPosition.y + 20, 115, 70 };
+    const Rectangle exitButton = { exitButtonPosition.x + 90, exitButtonPosition.y + 20, 115, 50 };
 
     Texture2D background = LoadTexture("../assets/inventory/chembenchZoomed/chembenchZoomed.png");
 
@@ -161,7 +161,7 @@ void displayChembenchZoomed()
 
         bool isMouseOverExitButton = CheckCollisionPointRec(mousePosition, exitButton);
 
-        DrawText("Exit", exitButtonPosition.x + 100, exitButtonPosition.y + 55, 50, isMouseOverExitButton ? BLACK : WHITE);
+        DrawText("Exit", exitButtonPosition.x + 100, exitButtonPosition.y + 25, 50, isMouseOverExitButton ? BLACK : WHITE);
 
         if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
@@ -191,95 +191,100 @@ void displayShelfInside()
     const int screenWidth = 1920;
     const int screenHeight = 1080;
 
-
     Image logo = LoadImage("../assets/logo/logoTransparent.png");
-
     SetWindowIcon(logo);
 
     Vector2 exitButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
 
-    Vector2 H2OButtonPosition = { GetScreenWidth() / 2 - 550, GetScreenHeight() / 2 - 330 };
-    Vector2 HClButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
-    Vector2 HNO3ButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
-    Vector2 NaOHButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
-    Vector2 CaOH2ButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
-    Vector2 NaClButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
-    Vector2 O2ButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
-    Vector2 H2ButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
-    Vector2 N2ButtonPosition = { GetScreenWidth() / 2 - 600, GetScreenHeight() / 2 + 100 };
-    Vector2 Cl2ButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
-    Vector2 NaButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
-    Vector2 CaButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
+    Vector2 buttonPositions[] = {
+        { GetScreenWidth() / 2 - 420, GetScreenHeight() / 2 - 380 }, // H2O
+        { GetScreenWidth() / 2 - 220, GetScreenHeight() / 2 - 380 }, // HCl
+        { GetScreenWidth() / 2 , GetScreenHeight() / 2 - 380 }, // HNO3
+        { GetScreenWidth() / 2 + 240, GetScreenHeight() / 2 - 400 }, // NaOH
+        { GetScreenWidth() / 2 - 390, GetScreenHeight() / 2 - 160 }, // CaOH2
+        { GetScreenWidth() / 2 - 170, GetScreenHeight() / 2 - 160 }, // NaCl
+        { GetScreenWidth() / 2 + 60, GetScreenHeight() / 2 - 160 },  // O2
+        { GetScreenWidth() / 2 + 325, GetScreenHeight() / 2 - 160 },  // H2
+        { GetScreenWidth() / 2 - 380, GetScreenHeight() / 2 + 120 }, // N2
+        { GetScreenWidth() / 2 - 170, GetScreenHeight() / 2 + 120 }, // Cl2
+        { GetScreenWidth() / 2 + 70, GetScreenHeight() / 2 + 150 }, // Na
+        { GetScreenWidth() / 2 + 300, GetScreenHeight() / 2 + 120 }  // Ca
+    };
 
     const Rectangle exitButton = { exitButtonPosition.x + 90, exitButtonPosition.y + 20, 115, 50 };
 
-    const Rectangle H2OButton = { H2OButtonPosition.x + 90, H2OButtonPosition.y + 20, 200, 100 };
-    const Rectangle HClButton = { HClButtonPosition.x + 90, HClButtonPosition.y + 20, 115, 50 };
-    const Rectangle HNO3Button = { HNO3ButtonPosition.x + 90, HNO3ButtonPosition.y + 20, 115, 50 };
-    const Rectangle NaOHButton = { NaOHButtonPosition.x + 90, NaOHButtonPosition.y + 20, 115, 50 };
-    const Rectangle CaOH2Button = { CaOH2ButtonPosition.x + 90, CaOH2ButtonPosition.y + 20, 115, 50 };
-    const Rectangle NaClButton = { NaClButtonPosition.x + 90, NaClButtonPosition.y + 20, 115, 50 };
-    const Rectangle O2Button = { O2ButtonPosition.x + 90, O2ButtonPosition.y + 20, 115, 50 };
-    const Rectangle H2Button = { H2ButtonPosition.x + 90, H2ButtonPosition.y + 20, 115, 50 };
-    const Rectangle N2Button = { N2ButtonPosition.x + 90, N2ButtonPosition.y + 20, 115, 50 };
-    const Rectangle Cl2Button = { Cl2ButtonPosition.x + 90, Cl2ButtonPosition.y + 20, 115, 50 };
-    const Rectangle NaButton = { NaButtonPosition.x + 90, NaButtonPosition.y + 20, 115, 50 };
-    const Rectangle CaButton = { CaButtonPosition.x + 90, CaButtonPosition.y + 20, 115, 50 };
-
+    const Rectangle buttons[] = {
+        { buttonPositions[0].x, buttonPositions[0].y, 200, 150 },
+        { buttonPositions[1].x, buttonPositions[1].y, 200, 150 },
+        { buttonPositions[2].x, buttonPositions[2].y, 200, 150 },
+        { buttonPositions[3].x, buttonPositions[3].y, 200, 150 },
+        { buttonPositions[4].x, buttonPositions[4].y, 130, 150 },
+        { buttonPositions[5].x, buttonPositions[5].y, 130, 150 },
+        { buttonPositions[6].x, buttonPositions[6].y, 130, 150 },
+        { buttonPositions[7].x, buttonPositions[7].y, 130, 150 },
+        { buttonPositions[8].x, buttonPositions[8].y, 150, 150 },
+        { buttonPositions[9].x, buttonPositions[9].y, 170, 150 },
+        { buttonPositions[10].x, buttonPositions[10].y, 130, 130 },
+        { buttonPositions[11].x, buttonPositions[11].y, 130, 150 }
+    };
 
     Texture2D background = LoadTexture("../assets/inventory/shelfInside.png");
+    Texture2D reagents[] = {
+        LoadTexture("../assets/inventory/info/H2OInfo.png"),
+        LoadTexture("../assets/inventory/info/HClInfo.png"),
+        LoadTexture("../assets/inventory/info/HNO3Info.png"),
+        LoadTexture("../assets/inventory/info/NaOHInfo.png"),
+        LoadTexture("../assets/inventory/info/CaOH2Info.png"),
+        LoadTexture("../assets/inventory/info/NaClInfo.png"),
+        LoadTexture("../assets/inventory/info/O2Info.png"),
+        LoadTexture("../assets/inventory/info/H2Info.png"),
+        LoadTexture("../assets/inventory/info/N2Info.png"),
+        LoadTexture("../assets/inventory/info/Cl2Info.png"),
+        LoadTexture("../assets/inventory/info/NaInfo.png"),
+        LoadTexture("../assets/inventory/info/CaInfo.png")
+    };
 
-    Texture2D H2O = LoadTexture("../assets/inventory/info/H2OInfo.png");
-    Texture2D HCl = LoadTexture("../assets/inventory/info/HClInfo.png");
-    Texture2D HNO3 = LoadTexture("../assets/inventory/info/HNO3Info.png");
-    Texture2D NaOH = LoadTexture("../assets/inventory/info/NaOHInfo.png");
-    Texture2D CaOH2 = LoadTexture("../assets/inventory/info/CaOH2Info.png");
-    Texture2D NaCl = LoadTexture("../assets/inventory/info/NaClInfo.png");
-    Texture2D O2 = LoadTexture("../assets/inventory/info/O2Info.png");
-    Texture2D H2 = LoadTexture("../assets/inventory/info/H2Info.png");
-    Texture2D N2 = LoadTexture("../assets/inventory/info/N2Info.png");
-    Texture2D Cl2 = LoadTexture("../assets/inventory/info/Cl2Info.png");
-    Texture2D Na = LoadTexture("../assets/inventory/info/NaInfo.png");
-    Texture2D Ca = LoadTexture("../assets/inventory/info/CaInfo.png");
+    Vector2 reagentPositions[] = {
+        { 55, 110 }, { 265, 100 }, { 495, 100 }, { 730, 100 },
+        { 70, 260 }, { 250, 260 }, { 485, 260 }, { 740, 260 },
+        { 35, 580 }, { 225, 580 }, { 510, 580 }, { 750, 580 }
+    };
 
+    int visibleReagentIndex = -1;
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         Vector2 mousePosition = GetMousePosition();
-        bool isH2OVisible = false;
-        bool isHClVisible = false;
 
-        if (CheckCollisionPointRec(mousePosition, H2OButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            isH2OVisible = true;
-        }
-        if (CheckCollisionPointRec(mousePosition, HClButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            isHClVisible = true;
-        }
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
 
         DrawTexture(background, 0, 0, RAYWHITE);
 
+        for (int i = 0; i < 12; i++)
+        {
+            bool isMouseOver = CheckCollisionPointRec(mousePosition, buttons[i]);
+
+            if (isMouseOver && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            {
+                visibleReagentIndex = i;
+            }
+        }
+
+        if (IsKeyDown(KEY_SPACE))
+        {
+            visibleReagentIndex = -1;
+        }
+
+        if (visibleReagentIndex != -1)
+        {
+            Vector2 pos = reagentPositions[visibleReagentIndex];
+            DrawTexture(reagents[visibleReagentIndex], pos.x, pos.y, RAYWHITE);
+        }
+
         bool isMouseOverExitButton = CheckCollisionPointRec(mousePosition, exitButton);
-
-        bool isMouseOverH2OButton = CheckCollisionPointRec(mousePosition, H2OButton);
-        bool isMouseOverHClButton = CheckCollisionPointRec(mousePosition, HClButton);
-        bool isMouseOverHNO3Button = CheckCollisionPointRec(mousePosition, HNO3Button);
-        bool isMouseOverNaOHButton = CheckCollisionPointRec(mousePosition, NaOHButton);
-        bool isMouseOverCaOH2Button = CheckCollisionPointRec(mousePosition, CaOH2Button);
-        bool isMouseOverNaClButton = CheckCollisionPointRec(mousePosition, NaClButton);
-        bool isMouseOverO2Button = CheckCollisionPointRec(mousePosition, O2Button);
-        bool isMouseOverH2Button = CheckCollisionPointRec(mousePosition, H2Button);
-        bool isMouseOverN2Button = CheckCollisionPointRec(mousePosition, N2Button);
-        bool isMouseOverCl2Button = CheckCollisionPointRec(mousePosition, Cl2Button);
-        bool isMouseOverNaButton = CheckCollisionPointRec(mousePosition, NaButton);
-        bool isMouseOverCaButton = CheckCollisionPointRec(mousePosition, CaButton);
-
 
         DrawText("Exit", exitButtonPosition.x + 100, exitButtonPosition.y + 25, 50, isMouseOverExitButton ? BLACK : WHITE);
 
@@ -288,72 +293,13 @@ void displayShelfInside()
             break;
         }
 
-        if (isH2OVisible)
-        {
-            DrawTexture(H2O, 400, 200, RAYWHITE); // Adjust position as needed
-        }
-        if (isHClVisible)
-        {
-            DrawTexture(HCl, HClButtonPosition.x, HClButtonPosition.y + 60, RAYWHITE);
-        }
-
-        if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            DrawTexture(H2O, H2OButtonPosition.x + 90, H2OButtonPosition.y + 20, RAYWHITE);
-        }
-        if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            DrawTexture(H2O, H2OButtonPosition.x + 90, H2OButtonPosition.y + 20, RAYWHITE);
-        }
-        if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            DrawTexture(H2O, H2OButtonPosition.x + 90, H2OButtonPosition.y + 20, RAYWHITE);
-        }
-        if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            DrawTexture(H2O, H2OButtonPosition.x + 90, H2OButtonPosition.y + 20, RAYWHITE);
-        }
-        if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            DrawTexture(H2O, H2OButtonPosition.x + 90, H2OButtonPosition.y + 20, RAYWHITE);
-        }
-        if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            DrawTexture(H2O, H2OButtonPosition.x + 90, H2OButtonPosition.y + 20, RAYWHITE);
-        }
-        if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            DrawTexture(H2O, H2OButtonPosition.x + 90, H2OButtonPosition.y + 20, RAYWHITE);
-        }
-        if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            DrawTexture(H2O, H2OButtonPosition.x + 90, H2OButtonPosition.y + 20, RAYWHITE);
-        }
-        if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            DrawTexture(H2O, H2OButtonPosition.x + 90, H2OButtonPosition.y + 20, RAYWHITE);
-        }
-        if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            DrawTexture(H2O, H2OButtonPosition.x + 90, H2OButtonPosition.y + 20, RAYWHITE);
-        }
-
         EndDrawing();
     }
 
     UnloadTexture(background);
-    UnloadTexture(H2O);
-    UnloadTexture(HCl);
-    UnloadTexture(HNO3);
-    UnloadTexture(NaOH);
-    UnloadTexture(CaOH2);
-    UnloadTexture(NaCl);
-    UnloadTexture(O2);
-    UnloadTexture(H2);
-    UnloadTexture(N2);
-    UnloadTexture(Cl2);
-    UnloadTexture(Na);
-    UnloadTexture(Ca);
+    for (int i = 0; i < 12; i++)
+    {
+        UnloadTexture(reagents[i]);
+    }
 }
-
 
