2eaa193260662e1981c79a740106dfdd11a64d41
ZVYankova22 <ZVYankova22@codingburgas.bg>
2025-01-04 02:18:04 +0200

    Display zoomed chembench and reagents, fix exit button

--- app/Runtimers/inventory.cpp
+++ app/Runtimers/inventory.cpp
@@ -99,6 +99,93 @@ void displayInventory()
     UnloadTexture(background);
 }
 
+void displayChembenchZoomed()
+{
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
+
+    Image logo = LoadImage("../assets/logo/logoTransparent.png");
+
+    SetWindowIcon(logo);
+
+    Vector2 exitButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
+
+    const Rectangle exitButton = { exitButtonPosition.x + 90, exitButtonPosition.y + 20, 115, 70 };
+
+    Texture2D background = LoadTexture("../assets/inventory/chembenchZoomed/chembenchZoomed.png");
+
+    Texture2D H2O = LoadTexture("../assets/inventory/chembenchZoomed/cards/H2OCard.png");
+    Texture2D HCl = LoadTexture("../assets/inventory/chembenchZoomed/cards/HClCard.png");
+    Texture2D HNO3 = LoadTexture("../assets/inventory/chembenchZoomed/cards/HNO3Card.png");
+    Texture2D NaOH = LoadTexture("../assets/inventory/chembenchZoomed/cards/NaOHCard.png");
+    Texture2D CaOH2 = LoadTexture("../assets/inventory/chembenchZoomed/cards/CaOH2Card.png");
+    Texture2D NaCl = LoadTexture("../assets/inventory/chembenchZoomed/cards/NaClCard.png");
+    Texture2D O2 = LoadTexture("../assets/inventory/chembenchZoomed/cards/O2Card.png");
+    Texture2D H2 = LoadTexture("../assets/inventory/chembenchZoomed/cards/H2Card.png");
+    Texture2D N2 = LoadTexture("../assets/inventory/chembenchZoomed/cards/N2Card.png");
+    Texture2D Cl2 = LoadTexture("../assets/inventory/chembenchZoomed/cards/Cl2Card.png");
+    Texture2D Na = LoadTexture("../assets/inventory/chembenchZoomed/cards/NaCard.png");
+    Texture2D Ca = LoadTexture("../assets/inventory/chembenchZoomed/cards/CaCard.png");
+
+
+    float x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, x7, y7, x8, y8, x9, y9, x10, y10, x11, y11, x12, y12;
+    x1 = 690, y1 = - 170, x2 = 840, y2 = - 170, x3 = 870, y3 = - 430, x4 = 490, y4 = - 228;
+    x5 = 820, y5 = - 235, x6 = 730, y6 = - 230, x7 = 865, y7 = - 230, x8 = 280, y8 = 62;
+    x9 = 610, y9 = - 10, x10 = 765, y10 = 135, x11 = 680, y11 = 383, x12 = 740, y12 = 210;
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        Vector2 mousePosition = GetMousePosition();
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawTexture(background, 0, 0, RAYWHITE);
+
+        DrawTexture(H2, x1, y1, RAYWHITE);
+        DrawTexture(H2O, x2, y2, RAYWHITE);
+        DrawTexture(O2, x3, y3, RAYWHITE);
+        DrawTexture(Cl2, x4, y4, RAYWHITE);
+        DrawTexture(N2, x5, y5, RAYWHITE);
+        DrawTexture(Ca, x6, y6, RAYWHITE);
+        DrawTexture(Na, x7, y7, RAYWHITE);
+        DrawTexture(HCl, x8, y8, RAYWHITE);
+        DrawTexture(NaOH, x9, y9, RAYWHITE);
+        DrawTexture(NaCl, x10, y10, RAYWHITE);
+        DrawTexture(HNO3, x11, y11, RAYWHITE);
+        DrawTexture(CaOH2, x12, y12, RAYWHITE);
+
+
+        bool isMouseOverExitButton = CheckCollisionPointRec(mousePosition, exitButton);
+
+        DrawText("Exit", exitButtonPosition.x + 100, exitButtonPosition.y + 55, 50, isMouseOverExitButton ? BLACK : WHITE);
+
+        if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            break;
+        }
+
+        EndDrawing();
+    }
+
+    UnloadTexture(background);
+    UnloadTexture(H2O);
+    UnloadTexture(HCl);
+    UnloadTexture(HNO3);
+    UnloadTexture(NaOH);
+    UnloadTexture(CaOH2);
+    UnloadTexture(NaCl);
+    UnloadTexture(O2);
+    UnloadTexture(H2);
+    UnloadTexture(N2);
+    UnloadTexture(Cl2);
+    UnloadTexture(Na);
+    UnloadTexture(Ca);
+}
+
 void displayShelfInside()
 {
     const int screenWidth = 1920;
@@ -270,45 +357,3 @@ void displayShelfInside()
 }
 
 
-void displayChembenchZoomed()
-{
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
-
-
-    Image logo = LoadImage("../assets/logo/logoTransparent.png");
-
-    SetWindowIcon(logo);
-
-    Vector2 exitButtonPosition = { GetScreenWidth() / 2 + 650, GetScreenHeight() / 2 + 425 };
-
-    const Rectangle exitButton = { exitButtonPosition.x + 90, exitButtonPosition.y + 20, 115, 70 };
-
-    Texture2D background = LoadTexture("../assets/inventory/chembenchZoomed/chembenchZoomed.png");
-
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose())
-    {
-        Vector2 mousePosition = GetMousePosition();
-
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawTexture(background, 0, 0, RAYWHITE);
-
-        bool isMouseOverExitButton = CheckCollisionPointRec(mousePosition, exitButton);
-
-        DrawText("Exit", exitButtonPosition.x + 100, exitButtonPosition.y + 55, 50, isMouseOverExitButton ? BLACK : WHITE);
-
-        if (CheckCollisionPointRec(mousePosition, exitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            break;
-        }
-
-        EndDrawing();
-    }
-
-    UnloadTexture(background);
-}
